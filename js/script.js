99276a67fb23a30b6bf060d6bdeccc42e7062d2c
Justin-Vision <97095919+Justindescode@users.noreply.github.com>
2024-04-17 12:24:48 -0500

    Added next and previous page functionality

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const global = {
   search: {
     term: "",
     type: "",
-    Page: 1,
+    page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     apiKey: "9a3f37d043d1057908fed070bd8336f7",
@@ -245,7 +246,11 @@ async function search() {
   global.search.term = urlParams.get("search-term");
 
   if (global.search.term !== "" && global.search.term !== null) {
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
 
     if (results.length === 0) {
       showAlert(`No results found`);
@@ -260,6 +265,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+  // Clear previous results
+  document.querySelector("#search-results").innerHTML = "";
+  document.querySelector("#search-results-heading").innerHTML = "";
+  document.querySelector("#pagination").innerHTML = "";
+
   results.forEach((result) => {
     const div = document.createElement("div");
     div.classList.add("card");
@@ -268,7 +278,7 @@ function displaySearchResults(results) {
             ${
               result.poster_path
                 ? `<img
-              src="https://image.tmdb.org/t/p/w500/${result.poster_path}"
+              src="https://image.tmdb.org/t/p/w500${result.poster_path}"
               class="card-img-top"
               alt="${
                 global.search.type === "movie" ? result.title : result.name
@@ -297,9 +307,52 @@ function displaySearchResults(results) {
           </div>
         `;
 
+    document.querySelector("#search-results-heading").innerHTML = `
+              <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+    `;
+
     document.querySelector("#search-results").appendChild(div);
   });
+
+  displayPagination();
 }
+
+const displayPagination = function () {
+  const div = document.createElement("div");
+  div.classList.add("pagination");
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">Page ${global.search.page}of ${global.search.totalPages}</div>
+</div>`;
+
+  document.querySelector("#pagination").appendChild(div);
+
+  //Disable pre button if on first page
+  if (global.search.page === 1) {
+    document.querySelector("#prev").disabled = true;
+  }
+
+  // Disable the next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector("#next").disabled = true;
+  }
+
+  //Next page
+  // Next page
+  document.querySelector("#next").addEventListener("click", async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  // Prev
+  document.querySelector("#prev").addEventListener("click", async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+};
 
 // Display Slider Movies
 async function displaySlider() {
@@ -372,7 +425,7 @@ async function searchAPIData() {
   showSpinner();
 
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   );
   const data = await response.json();
 
